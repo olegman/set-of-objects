23caf68ca7181a92555b3b18e9b963a3d6f0e2dd
Мартынов Олег Николаевич <oleg.martynov@ligastavok.ru>
2016-09-26 19:48:29 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# set-of-objects
+[![Build Status](https://img.shields.io/travis/olegman/set-of-objects/master.svg?style=flat-square)](https://travis-ci.org/olegman/set-of-objects)
+[![Latest Stable Version](https://img.shields.io/npm/v/set-of-objects.svg?style=flat-square)](https://www.npmjs.com/package/set-of-objects)
+[![Dependency Status](https://img.shields.io/david/olegman/set-of-objects.svg?style=flat-square)](https://david-dm.org/olegman/set-of-objects)
+[![License](https://img.shields.io/npm/l/set-of-objects.svg?style=flat-square)](https://www.npmjs.com/package/set-of-objects)
+[![NPM Downloads](https://img.shields.io/npm/dm/set-of-objects.svg?style=flat-square)](https://www.npmjs.com/package/set-of-objects)
+[![NPM Downloads](https://img.shields.io/npm/dt/set-of-objects.svg?style=flat-square)](https://www.npmjs.com/package/set-of-objects)
---
+++ src/index.js
@@ -0,0 +1,126 @@
+export function* enumerate(iterable) {
+    let i = 0;
+    for (const x of iterable) {
+        yield [x, i];
+        i++;
+    }
+}
+
+class Set {
+    constructor(items = [], options = {}) {
+        this.items = items;
+        if (options.make) this.__make = options.make;
+    }
+    *[Symbol.iterator]() {
+        let index = 0;
+        while (index in this.items) {
+            yield this.items[index];
+            index++;
+        }
+    }
+    static isValid(item) {
+        let result = false;
+        let error = null;
+        if (item.id === undefined) {
+            return {
+                result,
+                error: {
+                    code: 3,
+                    message: 'Item don\'t have required attribute id'
+                }
+            }
+        }
+        if (!(typeof item.id === 'number' || typeof item.id === 'string')) {
+            return {
+                result,
+                error: {
+                    code: 4,
+                    message: 'Item don\'t have proper type for attribute id, must be number or string'
+                }
+            }
+        }
+        return {
+            result: true,
+            error
+        }
+    }
+    add(item) {
+        let {result: validateResult, error: validateError} = Set.isValid(item);
+        if (validateResult) {
+            if (this.isAdded(item)) {
+                return {
+                    error: {
+                        code: 1,
+                        message: 'Item is already added'
+                    },
+                    result: null
+                }
+            } else {
+                this.items.push(item);
+                return {
+                    error: null,
+                    result: this.__make([...this.items])
+            }
+            }
+        } else {
+            return {
+                error: validateError,
+                result: null
+            }
+        }
+    }
+    isAdded({ id }) {
+        for (const item of this.items) {
+            if (item.id === id) return true;
+        }
+        return false;
+    }
+    remove({ id }) {
+        let isRemoved = false;
+        for (const [item, index] of enumerate(this.items)) {
+            if (item.id === id) {
+                this.items.splice(index, 1);
+                isRemoved = true;
+                break;
+            }
+        }
+        if (!isRemoved) {
+            return {
+                error: {
+                    code: 2,
+                    message: 'Not found to remove'
+                },
+                result: null
+            }
+        } else {
+            return {
+                error: null,
+                result: this.__make([...this.items])
+        }
+        }
+    }
+    removeById(id) {
+        return this.remove({ id });
+    }
+    removeAll() {
+        this.items = [];
+        return this.__make();
+    }
+    get(id) {
+        return this.items.find(item => item.id === id);
+    }
+    getByIndex(index) {
+        return this.items[index];
+    }
+    get length() {
+        return this.items.length
+    }
+}
+
+function makeSet(items = []) {
+    return new Set(items);
+}
+
+Set.prototype.__make = makeSet;
+
+export default Set;
